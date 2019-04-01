3bda489537b610768dcc29eaebadfb58b545967d
Nonso Mgbechi <mgbechinonso@gmail.com>
2019-04-01 09:04:17 +0100

    Created resource controller for posts and wrote code for the CRUD methods

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use App\User;
+use Illuminate\Http\Request;
+
+class PostsController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $posts = Post::orderBy('created_at', 'DESC')->get();
+        return view('posts.index', compact('posts'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $fields = $request->validate([
+            'title' => 'required',
+            'body' => 'required'
+        ]);
+        $fields['title'] = ucwords($fields['title']);
+        $fields['userId'] = auth()->id();
+        Post::create($fields);
+        return redirect()->route('posts.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Post $post)
+    {
+        return view('posts.show', compact('post'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Post $post)
+    {
+        return view('posts.edit', compact('post'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Post $post)
+    {
+        $fields = $request->validate([
+            'title' => 'required',
+            'body' => 'required'
+        ]);
+        $fields['title'] = ucwords($fields['title']);
+        $fields['userId'] = auth()->id();
+        $post->update($fields);
+        return view('posts.show', compact('post'));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Post  $posts
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Post $post)
+    {
+        $post->delete();
+        return redirect()->route('posts.index');
+    }
+}
