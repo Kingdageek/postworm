f2bdc5a0c47fa4772ba47c4199b362995038655f
Nonso Mgbechi <mgbechinonso@gmail.com>
2019-04-01 08:50:04 +0100

    Added other required user registration fields

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,9 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('firstName', 30);
+            $table->string('lastName', 30);
+            $table->string('username')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
